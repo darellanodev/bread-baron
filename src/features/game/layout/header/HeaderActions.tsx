3faa06ad0116c84f6f90dc9d848ab0f68610b1fc
darellanodev <darellano.aliaga@gmail.com>
2026-02-04 21:13:46 +0100

    fix: Center "Save Game" text button vertically

--- src/features/game/layout/header/HeaderActions.tsx
+++ src/features/game/layout/header/HeaderActions.tsx
@@ -3,7 +3,7 @@ import { SettingsMenu, RoundedButton } from '../../../../components/ui'
 export function HeaderActions() {
   return (
     <div className="flex gap-2">
-      <RoundedButton className="min-w-[84px] h-10 bg-primary text-textLight active:scale-95 rounded-full">
+      <RoundedButton className="min-w-[84px] h-10 bg-primary text-textLight active:scale-95 rounded-full flex items-center justify-center">
         Save Game
       </RoundedButton>
       <SettingsMenu />
