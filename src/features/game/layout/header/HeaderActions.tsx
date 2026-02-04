6c4c025f2ab5ae755292191bf3e0f10e8138bfb2
darellanodev <darellano.aliaga@gmail.com>
2026-02-04 21:15:58 +0100

    fix: Change "Save Game" to "Pause Game" label button

--- src/features/game/layout/header/HeaderActions.tsx
+++ src/features/game/layout/header/HeaderActions.tsx
@@ -4,7 +4,7 @@ export function HeaderActions() {
   return (
     <div className="flex gap-2">
       <RoundedButton className="min-w-[84px] h-10 bg-primary text-textLight active:scale-95 rounded-full flex items-center justify-center">
-        Save Game
+        Pause game
       </RoundedButton>
       <SettingsMenu />
     </div>
